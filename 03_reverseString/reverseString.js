10e23a36a40fbe56e2a520b16b545091cdd0aa0a
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-19 14:12:35 -0500

    03_reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(s1) {
+  s1 = s1.split('');
+  let s2 = [];
+  for (let i = s1.length -1; i>=0; i--){
+    s2.push(s1[i]);
+  }
+  return s2.join(''); // multiple line answer
+  // return s1.split('').reverse().join('');  // single line answer
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
