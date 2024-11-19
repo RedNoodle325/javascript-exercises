a3836b7ba2b0afe2e3485a89becf6fa1fce8b8c1
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-19 14:02:10 -0500

    02_repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, multiplier) {
+  let result = "";
+  for (let i = 0; i < multiplier; i++){
+    result += string;
+  };
+  return result;
 };
 
 // Do not edit below this line
