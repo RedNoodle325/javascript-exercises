61243f4b24e4d624721ede7e019f41b318855710
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-20 23:08:09 -0500

    finished 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  const tempC = (tempF - 32) * (5 / 9);
+  if (!Number.isInteger(tempC)) {
+    result = parseFloat(tempC.toFixed(1));
+  } else {
+    result = tempC;
+  }
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  const tempF = ((9 / 5) * tempC) + 32;
+  if (!Number.isInteger(tempF)) {
+    result = parseFloat(tempF.toFixed(1));
+  } else {
+    result = tempF;
+  }
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
