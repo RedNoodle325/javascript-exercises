ec7f6ad91e610f42de3f262e0c8032903e69948f
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-21 16:51:14 -0500

    finished 08 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+  let result = 0;
+  if (input.length === 0){
+    return result;
+  } else {
+    for (i=0; i<input.length; i++){
+        result += input[i];
+    }
+    return result;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  let result = 0;
+  if (input.length === 0){
+    return result;
+  } else {
+    result = 1;
+    for (i=0; i<input.length; i++){
+        result *= input[i];
+    }
+    return result;
+  }
 };
 
-const power = function() {
-	
+const power = function(num1, pow) {
+  let result = 1;
+  while(pow>0){
+    result *=num1;
+    pow--;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num <= 1 ){
+    return 1;
+  } else {
+    return num*factorial(num-1);
+  }
 };
 
 // Do not edit below this line
