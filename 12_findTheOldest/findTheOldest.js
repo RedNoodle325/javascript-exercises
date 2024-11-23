1a82de16230a0d831373c419381efef6c8352b70
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-22 23:48:26 -0500

    edited the findTheOldest to use currentyear from the Date function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,12 +2,13 @@ const findTheOldest = function (people) {
   let max = 0;
   let age = 0;
   let oldest = -1;
+  const currentYear = new Date().getFullYear();
 
   for (i = 0; i < people.length; i++) {
     if ('yearOfDeath' in people[i]){
       age = people[i].yearOfDeath - people[i].yearOfBirth;
     } else {
-      age = 2024 - people[i].yearOfBirth;
+      age = currentYear - people[i].yearOfBirth;
     }
 
     console.log(people[i].name + ": " + age);
