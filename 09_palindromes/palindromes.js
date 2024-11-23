1753eb31ae6d6e464804e661df03af38068b0927
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-22 21:51:03 -0500

    finished 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-  
-};
+const palindromes = function (letters) {
+  const letterReg = /[^A-Za-z0-9]/g;
+  const removeChar = letters.replace(letterReg, '').toLowerCase(); 
+  const characters = removeChar.split('');
+  console.log(removeChar);
+  let testing;
+  let i = 0, j = characters.length -1;
+  while(i<=j){
+      if (characters[i] === characters[j]) {
+        i++;
+        j--;
+      } else {
+        return false
+      }
+    }
+    return true;
+  }
 
 // Do not edit below this line
 module.exports = palindromes;
