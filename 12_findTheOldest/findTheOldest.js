8a36b58f54af97c115ec39906c36c812db2f5954
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-22 23:45:26 -0500

    finished it all!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let max = 0;
+  let age = 0;
+  let oldest = -1;
 
+  for (i = 0; i < people.length; i++) {
+    if ('yearOfDeath' in people[i]){
+      age = people[i].yearOfDeath - people[i].yearOfBirth;
+    } else {
+      age = 2024 - people[i].yearOfBirth;
+    }
+
+    console.log(people[i].name + ": " + age);
+    if (age > max) {
+      max = age;
+      oldest = i;
+    }
+  }
+  return people[oldest];
 };
 
 // Do not edit below this line
