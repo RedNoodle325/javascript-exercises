cc067aa46b1f192f65268407e4147f781260b99d
Zak Klinedinst <zak.klinedinst@proton.me>
2024-11-22 22:46:42 -0500

    finished 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  n = parseInt(n);
+  let result = 1, previous = 1; ans = 0;
 
-};
+  if (n<0){return "OOPS";};
+  if (n===1 || n===2){return 1;};
+  for (let i = 1; i < n-1; i++){
+    ans = result + previous;
+    previous = result;
+    result = ans;
+  }
+  return ans;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
